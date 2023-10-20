319e41917fa58e6765aefea05995d172e5682ccf
theamik <amik.sylhet@gmail.com>
2023-10-20 17:38:10 +0600

    598

--- api.js
+++ api.js
@@ -1,11 +1,11 @@
 const express =require('express');
-const dbConnect = require('../config/dbConnect');
+const dbConnect = require('./config/dbConnect');
 
 const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT||4000;
 const bodyParser = require('body-parser');
-const errorMiddleware = require("../middlewares/error");
+const errorMiddleware = require("./middlewares/error");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan")
 const cors = require("cors");
@@ -20,16 +20,16 @@ app.use(cookieParser());
 // app.use('/',(req,res)=>{
 //     res.send("Hello guys, its backend");
 // })
-const authRouter = require('../routes/AuthRoute');
-const productRouter = require('../routes/ProductRoute');
-const blogRouter = require('../routes/BlogRoute');
-const productCategoryRoouter = require('../routes/ProductCategoryRoute');
-const blogCatRouter = require('../routes/BlogCatRoute');
-const brandRouter = require('../routes/BrandRoute');
-const couponRouter = require('../routes/CouponRoute');
-const colorRouter = require('../routes/ColorRoute');
-const uploadRouter = require('../routes/UploadRoute');
-const enqRouter = require('../routes/EnqRoute');
+const authRouter = require('./routes/AuthRoute');
+const productRouter = require('./routes/ProductRoute');
+const blogRouter = require('./routes/BlogRoute');
+const productCategoryRoouter = require('./routes/ProductCategoryRoute');
+const blogCatRouter = require('./routes/BlogCatRoute');
+const brandRouter = require('./routes/BrandRoute');
+const couponRouter = require('./routes/CouponRoute');
+const colorRouter = require('./routes/ColorRoute');
+const uploadRouter = require('./routes/UploadRoute');
+const enqRouter = require('./routes/EnqRoute');
 
 app.use('/api/v1/user',authRouter);
 app.use('/api/v1/product',productRouter);
