0c3b38f2089fc3c7f03bb8b0aa97cf401fac32d3
theamik <amik.sylhet@gmail.com>
2023-10-20 18:08:10 +0600

    598

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '..',"/public/images/"));
+    cb(null, path.join(__dirname, '..',"public/images/"));
   },
   filename: function (req, file, cb) {
     const uniquesuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
@@ -35,7 +35,6 @@ const productImgResize = async (req, res, next) => {
         .toFormat("jpeg")
         .jpeg({ quality: 90 })
         .toFile(`public/images/${file.filename}`);
-      fs.unlinkSync(`public/images/${file.filename}`);
     })
   );
   next();
